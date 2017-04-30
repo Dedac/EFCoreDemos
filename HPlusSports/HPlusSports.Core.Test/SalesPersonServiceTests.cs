98ef68b610ec8c27ac7ca5f0c5cf3cc313494424
Richard Goforth <rcgoforth@gmail.com>
2017-04-30 17:01:08 -0400

    Add moq testing

--- HPlusSports/HPlusSports.Core.Test/SalesPersonServiceTests.cs
+++ HPlusSports/HPlusSports.Core.Test/SalesPersonServiceTests.cs
@@ -18,17 +18,38 @@ namespace HPlusSports.Core.Test
             var salesRepo = new SalesPersonRepositoryMock();
             var salesGroupRepo = new SalesGroupRepositoryMock();
 
-                salesGroupRepo.Add(new SalesGroup() { State = "TEST", Type = 1, Id = 1 });
-                salesRepo.Add(new Salesperson() { Id = 1 });
+            salesGroupRepo.Add(new SalesGroup() { State = "TEST", Type = 1, Id = 1 });
+            salesRepo.Add(new Salesperson() { Id = 1 });
 
-                var service = new SalesPersonService(salesRepo, salesGroupRepo);
+            var service = new SalesPersonService(salesRepo, salesGroupRepo);
 
-                await service.MoveSalesPersonToGroup(1, 1);
+            await service.MoveSalesPersonToGroup(1, 1);
 
-                var person = await salesRepo.GetByID(1);
-             
-                Assert.IsTrue(person.SalesGroup.State == "TEST");
+            var person = await salesRepo.GetByID(1);
+
+            Assert.IsTrue(person.SalesGroup.State == "TEST");
         }
 
+        [TestMethod]
+        public async Task MoveSalesPersonWithoutGroupToExistingGroupMoq()
+        {
+            var srMock = new Mock<ISalesPersonRepository>();
+            var sgrMock = new Mock<ITrackingRepository<SalesGroup>>();
+
+            var salesperson = new Salesperson() { Id = 1 };
+            sgrMock.Setup(x => x.GetByID(1)).Returns(Task.FromResult(new SalesGroup() { Id = 1, State = "TEST" }));
+
+            srMock.Setup(x => x.GetByID(1)).Returns(Task.FromResult(salesperson));
+
+            srMock.Setup(x => x.Save(It.IsAny<Salesperson>())).Callback<Salesperson>(p => salesperson = p);
+
+            var service = new SalesPersonService(srMock.Object, sgrMock.Object);
+
+            await service.MoveSalesPersonToGroup(1, 1);
+
+            var person = await srMock.Object.GetByID(1);
+
+            Assert.IsTrue(person.SalesGroup.State == "TEST");
+        }
     }
 }
